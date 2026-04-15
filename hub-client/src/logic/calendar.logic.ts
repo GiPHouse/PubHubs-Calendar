dd7bbf2416e67dacb38751d8f84ba090efba1a56
Tristan <271104tristan@gmail.com>
2026-04-15 17:19:13 +0200

    added functionallity to generate ical file

---
+++ hub-client/src/logic/calendar.logic.ts
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import ical from 'ical-generator';
+import os from 'os';
+import path from 'path';
+
+import { CalendarEvent } from '@hub-client/models/events/calendar/TCalendarEvent';
+
+export function generateIcsFromEvent(event: CalendarEvent): string {
+	const title = event.title;
+	const description = event.description;
+	const start = event.startTime;
+	const end = event.endTime;
+
+	if (!title) throw new Error('event.title is required');
+	if (!start) throw new Error('event.startTime is required');
+	if (!end) throw new Error('event.endTime is required');
+
+	const calendar = ical({ name: title });
+
+	calendar.createEvent({
+		start: start,
+		end: end,
+		description: description,
+		//location: ...,
+		//timezone: ...
+	});
+
+	const icsString = calendar.toString();
+	const fileName = title + '.ics';
+	const filePath = path.join(os.tmpdir(), fileName);
+	fs.writeFileSync(filePath, icsString, 'utf8');
+
+	return filePath;
+}
